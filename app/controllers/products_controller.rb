90438df43649d0b1d9f58548976d0103e387be0a
Jafet Aparicio <jafet.aparicio1@gmail.com>
2017-03-29 03:16:35 +0000

    working code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,38 +1,47 @@
 class ProductsController < ApplicationController
-  def index #show all products from the database
-    @products = Product.all
-  end
-  
-  def new
-  end
-  
-  def create #doesnt need view
-    @product = Product.new
-    @product.name = params[:names]
-    @product.description = params[:description]
-    @product.picture_url = params[:picture_url]
-    @product.price = params [:price]
-    @product.save
+    def index
+        @products = Product.all
+    end
     
-    redirect_to '/products'
-  end
-  
-  def edit
-    @product = Product.find params[:id]
-  end
-  
-  def update #doesnt need view
-    @product = Product.find params[:id]
-    @product.name = params[:names]
-    @product.description = params[:description]
-    @product.picture_url = params[:picture_url]
-    @product.price = params [:price]
-    @product.save
+    def new
+        @product = Product.new
+    end
     
-    redirect_to '/products'
-  end
-  
-  def show #have not seen before
-    @product = Product.find params[:id]
-  end
+    def create
+        @product = Product.new
+        @product.name = params[:name]
+        @product.description = params[:description]
+        @product.picture_url = params[:picture_url]
+        @product.price = params[:price]
+        @product.category_id = params[:category_id]
+        
+        if @product.save
+            redirect_to '/products'
+        else
+            render action: 'new'
+        end
+    end
+    
+    def edit
+        @product = Product.find params[:id]
+    end
+
+    def update
+        @product = Product.find params[:id]
+        @product.name = params[:name]
+        @product.description = params[:description]
+        @product.picture_url = params[:picture_url]
+        @product.price = params[:price]
+        @product.category_id = params[:category_id]
+        
+        if @product.save
+            redirect_to '/products'
+        else
+            render action: 'edit'
+        end
+    end
+    
+    def show
+        @product = Product.find params[:id]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html'
-  
-  get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new'
-  post '/products', to: 'products#create'
-  get '/products/:id/edit', to: 'products#edit'
-  put '/products/:id', to: 'products#update'
-  get 'products/:id', to: 'products#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/products', to: 'products#index'
+    get '/products/new', to: 'products#new'
+    post '/products', to: 'products#create'
+    get '/products/:id/edit', to: 'products#edit'
+    put '/products/:id', to: 'products#update'
+    get '/products/:id', to: 'products#show'
+    
+    get '/categories', to: 'categories#index'
+    get '/categories/new', to: 'categories#new'
+    post '/categories', to: 'categories#create'
+    get '/categories/:id/edit', to: 'categories#edit'
+    put '/categories/:id', to: 'categories#update'
 end
